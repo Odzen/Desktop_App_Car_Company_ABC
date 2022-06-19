9229ed338ff9ff88dc6b041a969fedf106616540
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-06-19 18:24:20 -0500

    Add files via upload
    cambiando a maven

---
+++ src/main/java/Modelo/Conexion.java
@@ -0,0 +1,74 @@
+package Modelo;
+
+// Clase para conectar con la base de datos en postgreSQL
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private String username = "";
+    private String password = "";
+    private String dbUrl = "";
+    private URI dbUri = URI.create("");
+    public static Connection con = null;
+
+    //private final URI dbUri = "postgres://frztblycjqgdss:39b247709a9689db8bb5671d4c65bdf8051ff7883fc5e931b9bd97d804bd19d1@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d3liet8hanl9f3";
+
+
+    public Conexion() {
+        try {
+            //this.dbUri = new URI(System.getenv("DATABASE_URL"));
+            this.dbUri = new URI("postgres://frztblycjqgdss:39b247709a9689db8bb5671d4c65bdf8051ff7883fc5e931b9bd97d804bd19d1@ec2-34-231-221-151.compute-1.amazonaws.com:5432/d3liet8hanl9f3");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.username = this.dbUri.getUserInfo().split(":")[0];
+        this.password = dbUri.getUserInfo().split(":")[1];
+        this.dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getDbUrl() {
+        return this.dbUrl;
+    }
+
+    public URI getDbUri() {
+        return dbUri;
+    }
+
+    public static Connection getCon() {
+        return con;
+    }
+
+    public Connection getConnection(){
+        try{
+            con = DriverManager.getConnection(this.getDbUrl(), this.getUsername(), this.getPassword());
+            System.out.println("Conexión Exitosa a la Base de Datos en Heroku!!\n");
+            return con;
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            getCon().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/empresaautosabc/EmpresaAutosABC.java
@@ -0,0 +1,48 @@
+package empresaautosabc;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+
+import java.io.IOException;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+/**
+ *
+ * @author mavel
+ */
+public class EmpresaAutosABC extends Application {
+
+    public void start(Stage stage) throws IOException  {
+        
+        System.out.println(getClass());
+        
+        //Ventana de Login
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vista/Login.fxml"));
+        Parent root = loader.load();
+        
+        // Escena para JavaFX
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+        new animatefx.animation.BounceIn(root).play();
+                
+    }
+   
+  /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+                
+}
