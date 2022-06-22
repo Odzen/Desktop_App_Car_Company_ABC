c151055d9b638869c414da9157e1f7cb35cee4ed
odzen <jsebastian.va@gmail.com>
2022-06-22 01:06:36 -0500

    Validaciones casi listas, formato de registro

--- src/main/java/org/openjfx/Controllers/RegistrarUsuarioController.java
+++ src/main/java/org/openjfx/Controllers/RegistrarUsuarioController.java
@@ -45,7 +45,7 @@ public class RegistrarUsuarioController implements Initializable {
     private TextField txtTelefono;
     @FXML
     private DatePicker dtpNacimiento;
-     @FXML
+    @FXML
     private SplitMenuButton cargo;
     @FXML
     MenuItem firstItem;
@@ -57,90 +57,62 @@ public class RegistrarUsuarioController implements Initializable {
     //Para validar los campos de usuario y contraseña
     @FXML
     protected void btnAgregarClick() throws IOException{
-    
+        validacionRegistroLabel.setText("");
+        txtNombre.setStyle(null);
+        txtPasswordConfirm.setStyle(null);
+        txtApellido.setStyle(null);
+        txtPassword.setStyle(null);
+        txtMail.setStyle(null);
+        txtDocumento.setStyle(null);
+        txtTelefono.setStyle(null);
+        dtpNacimiento.setStyle(null);
+        cargo.setStyle(null);
+        System.out.println("Presionó Confirmar");
         // Cuando los campos están en blanco
         if(txtNombre.getText().isEmpty() || txtPasswordConfirm.getText().isEmpty() ||
             txtApellido.getText().isEmpty() || txtPassword.getText().isEmpty() ||
             txtMail.getText().isEmpty()|| txtDocumento.getText().isEmpty() ||
-            txtTelefono.getText().isEmpty() || dtpNacimiento.getAccessibleText().isEmpty() ||
-            cargo.getText().isEmpty())
+            txtTelefono.getText().isEmpty() || dtpNacimiento.getValue()==null ||
+            cargo.getText().equals("Seleccionar Cargo"))
         {
-                validacionRegistroLabel.setStyle(mensajeError);
-                if(txtNombre.getText().isEmpty() && txtPasswordConfirm.getText().isEmpty() &&
-                    txtApellido.getText().isEmpty() && txtPassword.getText().isEmpty() &&
-                    txtMail.getText().isEmpty()  && txtDocumento.getText().isEmpty() &&
-                    txtTelefono.getText().isEmpty() && dtpNacimiento.getAccessibleText().isEmpty() &&
-                    cargo.getText().isEmpty())
-                {
-                    validacionRegistroLabel.setText("Se requieren todos los campos!");
-                    txtNombre.setStyle(mensajeError);
-                    txtPasswordConfirm.setStyle(mensajeError);
-                    txtApellido.setStyle(mensajeError);
-                    txtPassword.setStyle(mensajeError);
-                    txtMail.setStyle(mensajeError);
-                    txtDocumento.setStyle(mensajeError);
-                    txtTelefono.setStyle(mensajeError);
-                    dtpNacimiento.setStyle(mensajeError);
-                    cargo.setStyle(mensajeError);
-                    new Shake(txtNombre).play();
-                    new Shake(txtPasswordConfirm).play();
-                    new Shake(txtApellido).play();
-                    new Shake(txtPassword).play();
-                    new Shake(txtMail).play();
-                    new Shake(txtDocumento).play();
-                    new Shake(txtTelefono).play();
-                    new Shake(dtpNacimiento).play();
-                    new Shake(cargo).play();
+            validacionRegistroLabel.setStyle(mensajeError);
+            if(txtNombre.getText().isEmpty() && txtPasswordConfirm.getText().isEmpty() &&
+                txtApellido.getText().isEmpty() && txtPassword.getText().isEmpty() &&
+                txtMail.getText().isEmpty()  && txtDocumento.getText().isEmpty() &&
+                txtTelefono.getText().isEmpty() && dtpNacimiento.getValue()==null &&
+                cargo.getText().equals("Seleccionar Cargo"))
+            {
+                validacionRegistroLabel.setText("Se requieren todos los campos!");
+                txtNombre.setStyle(estiloMensajeError);
+                txtPasswordConfirm.setStyle(estiloMensajeError);
+                txtApellido.setStyle(estiloMensajeError);
+                txtPassword.setStyle(estiloMensajeError);
+                txtMail.setStyle(estiloMensajeError);
+                txtDocumento.setStyle(estiloMensajeError);
+                txtTelefono.setStyle(estiloMensajeError);
+                dtpNacimiento.setStyle(estiloMensajeError);
+                cargo.setStyle(estiloMensajeError);
+                new Shake(txtNombre).play();
+                new Shake(txtPasswordConfirm).play();
+                new Shake(txtApellido).play();
+                new Shake(txtPassword).play();
+                new Shake(txtMail).play();
+                new Shake(txtDocumento).play();
+                new Shake(txtTelefono).play();
+                new Shake(dtpNacimiento).play();
+                new Shake(cargo).play();
+            } else {
+                validacionRegistroLabel.setText("Algunos campos están vacíos!");
+                boolean validado = this.validaciones();
+                if (validado) {
+                    this.validadoLabelSet();
                 }
-        }
-        // Validacion contraseña
-        else if (!Validaciones.validarPassword(txtPassword.getText()))
-        {
-            validacionRegistroLabel.setText("Formato de contraseña incorrecto!");
-            validacionRegistroLabel.setStyle(mensajeError);
-            txtPassword.setStyle(estiloMensajeError);
-            new FadeIn(txtPassword).play();
-        }
-        // Validacion confirmación de contraseña
-        else if (!Validaciones.validarPassword(txtPasswordConfirm.getText()))
-        {
-            validacionRegistroLabel.setText("Formato de contraseña incorrecto!");
-            validacionRegistroLabel.setStyle(mensajeError);
-            txtPasswordConfirm.setStyle(estiloMensajeError);
-            new FadeIn(txtPasswordConfirm).play();
-        }
-        // Validacion de telefono
-        else if (!Validaciones.validarTelefono(txtTelefono.getText()))
-        {
-            validacionRegistroLabel.setText("Formato de telefono incorrecto!");
-            validacionRegistroLabel.setStyle(mensajeError);
-            txtTelefono.setStyle(estiloMensajeError);
-            new FadeIn(txtTelefono).play();
-        }
-        // Se comprueba la longitud del nombre del usuario
-        else if (txtNombre.getText().length() < 6 ||  txtNombre.getText().length() > 20)
-        {
-            validacionRegistroLabel.setText("El usuario debe tener de 6 a 20 caracteres!");
-            validacionRegistroLabel.setStyle(mensajeError);
-            txtNombre.setStyle(estiloMensajeError);
-            new FadeIn(txtNombre).play();
-        }
-        // Validación Email
-        else if (Validaciones.validarEmail(txtMail.getText()))
-        {
-            validacionRegistroLabel.setText("Formato de email incorrecto!");
-            validacionRegistroLabel.setStyle(mensajeError);
-            txtMail.setStyle(estiloMensajeError);
-            new FadeIn(txtMail).play();
-        }
-        // Mensaje si el ingreso es correcto
-        else
-        {
-            validacionRegistroLabel.setText("Ingreso éxitoso!");
-            validacionRegistroLabel.setStyle(mensajeExito);
-            txtNombre.setStyle(estiloMensajeExito);
-            txtPassword.setStyle(estiloMensajeExito);
-            new Tada(validacionRegistroLabel).play();
+            }
+        } else {
+            boolean validado = this.validaciones();
+            if (validado) {
+                this.validadoLabelSet();
+            }
         }
     }
 
@@ -158,17 +130,135 @@ public class RegistrarUsuarioController implements Initializable {
         EmpresaAutosABC.setRoot("menu");
     }
     @FXML
-    private void setFirstItem() throws IOException {
+    private void setFirstItem() {
         cargo.setText(firstItem.getText());
     }
 
     @FXML
-    private void setSecondItem() throws IOException {
+    private void setSecondItem() {
         cargo.setText(secondItem.getText());
+    }
+
+    private boolean validaciones() {
+        // Validacion contraseña
+        boolean validado = true;
+        validacionRegistroLabel.setText("");
+        if (!Validaciones.validarPassword(txtPassword.getText()))
+        {
+            validado = false;
+            String textoError = "Formato de contraseña incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtPassword.setStyle(estiloMensajeError);
+            new FadeIn(txtPassword).play();
+        }
+        // Validacion confirmación de contraseña
+        if (!Validaciones.validarPassword(txtPasswordConfirm.getText()))
+        {
+            validado = false;
+            String textoError = "Formato de confirmación de contraseña incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtPasswordConfirm.setStyle(estiloMensajeError);
+            new FadeIn(txtPasswordConfirm).play();
+        }
+        // Validacion de telefono
+        if (!Validaciones.validarTelefono(txtTelefono.getText()))
+        {
+            validado = false;
+            String textoError = "Formato de telefono incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtTelefono.setStyle(estiloMensajeError);
+            new FadeIn(txtTelefono).play();
+        }
+        // Se comprueba la longitud del nombre del usuario
+        if (txtNombre.getText().length() < 4 ||  txtNombre.getText().length() > 20)
+        {
+            validado = false;
+            String textoError = "El usuario debe tener de 4 a 20 caracteres!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtNombre.setStyle(estiloMensajeError);
+            new FadeIn(txtNombre).play();
+        }
+        // Se comprueba la longitud del nombre del usuario
+        if (txtApellido.getText().length() < 4 ||  txtApellido.getText().length() > 20)
+        {
+            validado = false;
+            String textoError = "El apellido debe tener de 4 a 20 caracteres!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtApellido.setStyle(estiloMensajeError);
+            new FadeIn(txtApellido).play();
+        }
+        // Se comprueba la longitud de la cedula del usuario
+        if (txtDocumento.getText().length() < 10)
+        {
+            validado = false;
+            String textoError = "La cedula debe de tener al menos 10 caracteres!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtDocumento.setStyle(estiloMensajeError);
+            new FadeIn(txtDocumento).play();
+        }
+        // Validación Email
+        if (!Validaciones.validarEmail(txtMail.getText()))
+        {
+            validado = false;
+            String textoError = "Formato de email incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            txtMail.setStyle(estiloMensajeError);
+            new FadeIn(txtMail).play();
+        }
+        // Validación Fecha
+        if (dtpNacimiento.getValue()==null)
+        {
+            validado = false;
+            String textoError = "Formato de fecha incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            dtpNacimiento.setStyle(estiloMensajeError);
+            new FadeIn(dtpNacimiento).play();
+        }
+        // Validacion Cargo
+        if (cargo.getText().equals("Seleccionar Cargo") || !cargo.getText().equals("Gerente") || !cargo.getText().equals("Administrador") )
+        {
+            validado = false;
+            String textoError = "Formato de cargo incorrecto!";
+            System.out.println(textoError);
+            validacionRegistroLabel.setText(validacionRegistroLabel.getText() + textoError + '\n');
+            validacionRegistroLabel.setStyle(mensajeError);
+            cargo.setStyle(estiloMensajeError);
+            new FadeIn(cargo).play();
+        }
+        // Mensaje si el ingreso es correcto
+        return validado;
+    }
+
+    private void validadoLabelSet() {
+        validacionRegistroLabel.setText("");
+        System.out.println("Pasó Validaciones");
+        validacionRegistroLabel.setStyle(mensajeExito);
+        validacionRegistroLabel.setText("Ingreso éxitoso!");
+        txtNombre.setStyle(estiloMensajeExito);
+        txtPassword.setStyle(estiloMensajeExito);
+        new Tada(validacionRegistroLabel).play();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
     }
+
 
 }
