347869397c27623af3c48b17c7cba506965bf902
MavelSterling <mavelynsl@gmail.com>
2022-07-22 05:21:36 -0500

    Se agregan validaciones para los autos

--- src/main/java/org/openjfx/Models/Automovil/Utils/ValidacionesAutomovil.java
+++ src/main/java/org/openjfx/Models/Automovil/Utils/ValidacionesAutomovil.java
@@ -13,13 +13,23 @@ public class ValidacionesAutomovil {
             Pattern.compile("^[0-9]{3,6}$", Pattern.CASE_INSENSITIVE);
 
     public static final Pattern VALID_MODELO =
-            Pattern.compile("^[a-z A-Z 0-9]{1,6}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z a-z 0-9]{1,6}$", Pattern.CASE_INSENSITIVE);
 
     public static final Pattern VALID_MARCA =
-            Pattern.compile("^[a-z A-Z]{4,20}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z a-z]{3,20}$", Pattern.CASE_INSENSITIVE);
 
     public static final Pattern VALID_PLACA =
-            Pattern.compile("^[A-Z]{3}\\-[0-9]{3}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[A-Z]{3}-[0-9]{3}$", Pattern.CASE_INSENSITIVE);
+
+    public static final Pattern VALID_COLOR =
+            Pattern.compile("^[A-Z a-z]{2,20}$", Pattern.CASE_INSENSITIVE);
+
+    public static final Pattern VALID_AÑO =
+            Pattern.compile("[0-9]{4}$", Pattern.CASE_INSENSITIVE);
+
+    public static final Pattern VALID_PRECIO =
+            Pattern.compile("[0-9]{8,9}$", Pattern.CASE_INSENSITIVE);
+
 
     public static final Pattern VALID_DATE =
             Pattern.compile("^((?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:(?:0[13578]|1[02])(-)31)|((0[1,3-9]|1[0-2])(-)(29|30))))$|^(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(-)02(-)29)$|^(?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:0[1-9])|(?:1[0-2]))(-)(?:0[1-9]|1\\d|2[0-8])$", Pattern.CASE_INSENSITIVE);
@@ -38,12 +48,18 @@ public class ValidacionesAutomovil {
         return matcher.find();
     }
 
-    // Verifica si la ciudad es correcta
+    // Verifica si la marca es correcta
     public static boolean validarMarca(String marca) {
         Matcher matcher = VALID_MARCA.matcher(marca);
         return matcher.find();
     }
 
+
+    // Verifica si la color es correcta
+    public static boolean validarColor(String color) {
+        Matcher matcher = VALID_COLOR.matcher(color);
+        return matcher.find();
+    }
 
     // Verifica si la PLACA es correcta
     public static boolean validarPlaca(String placa) {
@@ -57,13 +73,18 @@ public class ValidacionesAutomovil {
         return matcher.find();
     }
 
-    // Verifica si el telefono es correcto
-
-    String[] validPhoneNumbers
-            = {"2055550125","202 555 0125", "(202) 555-0125", "+111 (202) 555-0125",
-            "636 856 789", "+111 636 856 789", "636 85 67 89", "+111 636 85 67 89"};
-    public static boolean validarTelefono(String telefono) {
-        Matcher matcher = VALID_PHONE_NUMBER_REGEX.matcher(telefono);
+    // Verifica si la PLACA es correcta
+    public static boolean validarAño(String año) {
+        Matcher matcher = VALID_AÑO.matcher(año);
         return matcher.find();
     }
+    // Verifica si el cilindraje es correcto
+    public static boolean validarPrecio(String precio) {
+        Matcher matcher = VALID_PRECIO.matcher(precio);
+        return matcher.find();
+    }
+
+
+
+
 }
