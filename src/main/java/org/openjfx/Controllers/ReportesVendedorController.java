eed104d349d653cc8c7c268f75dd5f56f845c62e
Odzen <jsebastian.va@gmail.com>
2022-07-27 12:13:11 -0500

    Reporte de vendedores done

--- src/main/java/org/openjfx/Controllers/ReportesVendedorController.java
+++ src/main/java/org/openjfx/Controllers/ReportesVendedorController.java
@@ -20,7 +20,7 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.ResourceBundle;
 
-public class ReportesJefeController  implements Initializable {
+public class ReportesVendedorController  implements Initializable {
     @FXML
     private TableView<Automovil> tableGestionAutomovil;
     @FXML
@@ -50,102 +50,6 @@ public class ReportesJefeController  implements Initializable {
     private TableColumn<Automovil, String> col_Sede_Auto;
 
     private ObservableList<Automovil> automovilList = FXCollections.observableArrayList();
-    @FXML
-    private TableView<Cliente> tableGestionVendedor;
-    @FXML
-    private TableColumn<Cliente, Boolean> col_activoGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_apellidoGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_cedula_clienteGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_creadoPorGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_direccionGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_emailGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, Date> col_fecha_creacionGestionVendedor1;
-
-    @FXML
-    private TableColumn<Cliente, Date> col_fecha_modificadoGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, Integer> col_tipo_usuarioGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, Date> col_nacimientoGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_nombreGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_sedeGestionVendedor;
-
-    @FXML
-    private TableColumn<Cliente, String> col_telefonoGestionVendedor;
-
-
-    private ObservableList<Cliente> clientesList = FXCollections.observableArrayList();
-
-    private void refreshTableCliente() {
-        clientesList.clear();
-        this.readCliente();
-    }
-
-    private void loadDataCliente() {
-        this.refreshTableCliente();
-
-        col_cedula_clienteGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("cedula_cliente"));
-        col_emailGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("email"));
-        col_nombreGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        col_apellidoGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("apellido"));
-        col_fecha_modificadoGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("fecha_modificado"));
-        col_fecha_creacionGestionVendedor1.setCellValueFactory(new PropertyValueFactory<>("fecha_creacion"));
-        col_direccionGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("direccion"));
-        col_activoGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("activo"));
-        col_nacimientoGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("fecha_nacimiento"));
-        col_telefonoGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("telefono"));
-        col_tipo_usuarioGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("user_type"));
-        col_creadoPorGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("cedula_creado_por"));
-        col_sedeGestionVendedor.setCellValueFactory(new PropertyValueFactory<>("sede"));
-        tableGestionVendedor.setItems(clientesList.sorted());
-
-    }
-
-    private void readCliente() {
-        try {
-            ResultSet result = SQL_Cliente.obtenerTodosClienteSet();
-            while (result.next()) {
-                Cliente readCliente = new Cliente();
-
-                readCliente.setCedula_cliente(result.getString("cedula_cliente"));
-                readCliente.setEmail(result.getString("email"));
-                readCliente.setNombre(result.getString("nombre"));
-                readCliente.setApellido(result.getString("apellido"));
-                readCliente.setFecha_modificado(result.getDate("fecha_modificado"));
-                readCliente.setFecha_creacion(result.getDate("fecha_creacion"));
-                readCliente.setDireccion(result.getString("direccion"));
-                readCliente.setActivo(result.getBoolean("activo"));
-                readCliente.setFecha_nacimiento(result.getDate("fecha_nacimiento"));
-                readCliente.setTelefono(result.getString("telefono"));
-                readCliente.setId_tipo_usuario(result.getInt("id_tipo_usuario"));
-                readCliente.setCedula_creado_por(result.getString("cedula_creado_por"));
-                readCliente.setSede(result.getString("sede"));
-
-                clientesList.add(readCliente);
-            }
-            clientesList.sorted();
-        } catch(SQLException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
 
 
     private void refreshTableAuto() {
@@ -202,7 +106,7 @@ public class ReportesJefeController  implements Initializable {
 
     @FXML
     protected void btnInicio() throws IOException {
-        EmpresaAutosABC.setRoot("menuJefeTaller");
+        EmpresaAutosABC.setRoot("menuVendedor");
     }
 
     /**
@@ -210,9 +114,6 @@ public class ReportesJefeController  implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        this.readCliente();
-        this.loadDataCliente();
-        tableGestionVendedor.setItems(clientesList.sorted());
 
         this.readAuto();
         this.loadDataAuto();
